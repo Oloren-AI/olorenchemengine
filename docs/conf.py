f1ad424038d1af2fb2d92ce19b653a102a17098b
Andy Zhu <andy@oloren.ai>
2022-09-01 21:41:04 +0000

    sphinx google analytics

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,9 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinxcontrib.googleanalytics",
 ]
+googleanalytics_id = 'G-R5E6ME01BB'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
