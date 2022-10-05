58f3d4acf4288414f93005ecc8367bc0e7ce9dfc
David Huang <david@oloren.ai>
2022-10-05 18:00:22 +0000

    updating test_TorchGeometric

--- tests/test_known_errors.py
+++ tests/test_known_errors.py
@@ -16,17 +16,13 @@ def example_data1():
 #RuntimeError: Not compiled with CUDA support
 def test_TorchGeometric(example_data1):
     data = oce.RandomSplit(split_proportions=[0.8 ,0.0, 0.2]).split(example_data1)
-    train, test = data[0], data[1]
+    train, valid, test = data[0], data[1], data[2]
     s = "{'BC_class_name': 'BaseTorchGeometricModel', 'args': [{'BC_class_name': 'GINModel', 'args': [], 'kwargs': {'batch_size': 100, 'conv_radius': 3, 'conv_type': 'gin+', 'dataset': 'molpcba', 'dropout': 0.5, 'hidden': 100, 'layers': 3, 'lr': 0.001, 'optim': 'adam', 'task_type': 'classification', 'virtual_node': False}}], 'kwargs': {'auto_lr_find': True, 'batch_size': 16, 'epochs': 5, 'log': True, 'lr': 0.0001, 'pos_weight': 'balanced', 'preinitialized': False, 'representation': {'BC_class_name': 'TorchGeometricGraph', 'args': [], 'kwargs': {'atom_featurizer': {'BC_class_name': 'OGBAtomFeaturizer', 'args': [], 'kwargs': {}}, 'bond_featurizer': {'BC_class_name': 'OGBBondFeaturizer', 'args': [], 'kwargs': {}}}}}}"
     s = s.replace("'", "\"").replace("True", "true").replace("False", "false").replace("None", "null")
     model=(oce.create_BC(s))
 
-    try:
-        model.fit(train['Smiles'], train['pChEMBL Value'])
-        preds=model.predict(test['Smiles'])
-        assert(True)
-    except:
-        assert(False)
+    model.fit(train['Smiles'], train['pChEMBL Value'])
+    preds=model.predict(test['Smiles'])
 
 #TypeError: unsupported operand type(s) for +: 'Mol2Vec' and 'DescriptastorusDescriptor'
 def test_ConcatMol2Vec():
