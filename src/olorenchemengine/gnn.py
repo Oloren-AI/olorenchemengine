e0eb5295c14b9c6c6d2c31675828c086ca5718f6
David Huang <david@oloren.ai>
2022-10-19 02:07:31 +0000

    small hyper changes

--- src/olorenchemengine/gnn.py
+++ src/olorenchemengine/gnn.py
@@ -272,7 +272,10 @@ class BaseTorchGeometricModel(BaseModel):
         from pytorch_lightning import Trainer
 
         self.trainer = Trainer(
-            accelerator="auto", max_epochs=self.epochs, auto_lr_find=auto_lr_find, num_sanity_val_steps=0
+            accelerator="auto", 
+            max_epochs=self.epochs, 
+            auto_lr_find=auto_lr_find,
+            num_sanity_val_steps=0
         )
 
         super().__init__(log=False, **kwargs)
@@ -302,16 +305,9 @@ class BaseTorchGeometricModel(BaseModel):
         else:
             self.network.set_task_type("regression")
 
-        if self.network.hascollate_fn:
-            from torch.utils.data import DataLoader as TorchDataLoader
+        from torch_geometric.data import DataLoader as PyGDataLoader
 
-            dataloader = TorchDataLoader(
-                X_train, batch_size=self.batch_size, shuffle=True, num_workers=8, collate_fn=self.network.collate_fn
-            )
-        else:
-            from torch_geometric.data import DataLoader as PyGDataLoader
-
-            dataloader = PyGDataLoader(X_train, batch_size=self.batch_size, shuffle=True, num_workers=8)
+        dataloader = PyGDataLoader(X_train, batch_size=self.batch_size, shuffle=True, num_workers=oce.CONFIG["NUM_WORKERS"])
 
         self.trainer.fit(self.network, dataloader)
 
